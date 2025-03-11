05b398f9839b9027fb8dcd008ffc28f655df12f1
Framja <framja.app@gmail.com>
2025-03-11 16:24:14 -0300

    Atualização form-2

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
+  standalone: true,
   selector: 'app-home',
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
@@ -17,4 +21,26 @@ export class HomeComponent {
     this.stepAtual = step;
   }
 
-}
+   leadForm: FormGroup
+   constructor(private fb: FormBuilder) {
+    this.leadForm = this.fb.group({
+      nome: ['', [Validators.required]],
+      whatsapp: ['', [Validators.required, Validators.minLength(10)]],
+      empresa: ['', [Validators.required]],
+      segmento: ['', [Validators.required]],
+      cargo: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(8)]]
+
+
+    })
+   }
+  
+   onSubmit () {
+    if (this.leadForm.valid) {
+      console.log('Login Successful', this.leadForm.value);
+    }
+   }
+
+  
+  }
