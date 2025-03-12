acf07f862ab2c01b5858063d132521c628ccadaf
Framja <framja.app@gmail.com>
2025-03-12 18:43:02 -0300

    Subindo código com as alterações no formulário

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,27 +2,72 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AlertService } from '../../services/alert.service';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 @Component({
   standalone: true,
   selector: 'app-home',
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgxMaskDirective
   ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  providers: [provideNgxMask()],
 })
 export class HomeComponent {
 
-  stepAtual = "step1";
+  stepAtual = "step3";
+  exibirSenha: boolean = false;
 
   definirStep(step: string) {
     this.stepAtual = step;
   }
 
-   leadForm: FormGroup
-   constructor(private fb: FormBuilder) {
+  avancarStep2(): void {
+
+    const nomeValido = this.validarFormControl('nome');
+    const whatsappValido = this.validarFormControl('whatsapp');
+    if (nomeValido && whatsappValido) {
+      this.leadForm.markAsUntouched();
+      this.stepAtual = 'step2';
+
+    } else {
+      this.leadForm.markAllAsTouched();
+      this.alertService.exibirAvisoCampoObrigatorio();
+    }
+
+  }
+
+  avancarStep3(): void {
+
+    debugger;
+    const empresaValido = this.validarFormControl('empresa');
+    const segmentoValido = this.validarFormControl('segmento');
+    const cargoValido = this.validarFormControl('cargo');
+    if (empresaValido && segmentoValido && cargoValido) {
+      this.leadForm.markAsUntouched();
+      this.stepAtual = 'step3';
+
+    } else {
+      this.leadForm.markAllAsTouched();
+      this.alertService.exibirAvisoCampoObrigatorio();
+    }
+
+  }
+
+  voltarStep(step: string) {
+    this.stepAtual = step;
+  }
+
+  leadForm: FormGroup
+  constructor(
+    private fb: FormBuilder,
+    private alertService: AlertService,
+  ) {
+
     this.leadForm = this.fb.group({
       nome: ['', [Validators.required]],
       whatsapp: ['', [Validators.required, Validators.minLength(10)]],
@@ -30,17 +75,36 @@ export class HomeComponent {
       segmento: ['', [Validators.required]],
       cargo: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
-      senha: ['', [Validators.required, Validators.minLength(8)]]
+      senha: ['', [Validators.required]]
 
 
     })
-   }
-  
-   onSubmit () {
-    if (this.leadForm.valid) {
-      console.log('Login Successful', this.leadForm.value);
-    }
-   }
-
-  
   }
+
+  enviar (): void {
+
+    const emailValido = this.validarFormControl('email');
+    const senhaValido = this.validarFormControl('senha');
+    if (emailValido && senhaValido) {
+      
+      alert('gravou lead')
+
+    } else {
+      this.leadForm.markAllAsTouched();
+      this.alertService.exibirAvisoCampoObrigatorio();
+    }
+
+
+
+
+  }
+
+  validarFormControl(nomeControle: string): boolean {
+    return this.leadForm.controls[nomeControle].valid;
+  }
+
+  invalidarFormControl(nomeControle: string): boolean {
+    return this.leadForm.controls[nomeControle].invalid && (this.leadForm.controls[nomeControle].touched);
+  }
+
+}
---
+++ src/app/services/alert.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import Swal from 'sweetalert2';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+
+  constructor() { }
+
+
+  exibirAvisoCampoObrigatorio(): void {
+    Swal.fire({
+            text: "Por favor, preencha os campos obrigatórios.",
+            icon: "warning",
+            confirmButtonColor: "#820ADF",
+            iconColor: "grey"
+          });
+  }
+}
